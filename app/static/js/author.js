8b10eee411e6f3aec090d06729e9104bfa493893
alexmula11 <alexmula11@gmail.com>
2020-06-05 00:53:30 +0200

    Add participant beta

--- app/static/js/author.js
+++ app/static/js/author.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
 
-    var isLoggedIn = window.sessionStorage .getItem('loggedin');
     var username = window.sessionStorage .getItem('username');
     var loginas = window.sessionStorage .getItem('loginas');
 
@@ -38,12 +37,73 @@ $(document).ready(function() {
     });
 
     $('#ConferencesJoined').on('click', function(e) {
-    e.preventDefault();
+        e.preventDefault();
         var EventID =  Number.parseInt($( "#conferencesjoined").value)
         window.sessionStorage.setItem('EventID', EventID);
         var url = "http://localhost:8080/author/acceptedconf";
         $(location).attr('href',url);
 
+    });
+
+    $.ajax({
+        method: "GET",
+        url: '/getallconferences',
+
+        data: {
+            'username':username,
+            'loginas': loginas
+        },
+
+        success: function(data) {
+            console.log(data)
+            let dropdown = $('#conferencestojoin');
+
+            dropdown.empty();
+
+            dropdown.append('<option selected="true" disabled>Choose Conference</option>');
+            dropdown.prop('selectedIndex', 0);
+            $.each(data, function (key, entry) {
+            dropdown.append($('<option></option>').attr('value', entry.EventID).text(entry.Name));
+           });
+        },
+        statusCode: {
+            400: function() {
+                $('#msg').html('<span style="color: red;">Bad request - invalid credentials</span>');
+            }
+        },
+        error: function(err) {
+            console.log(err);
+        }
+    });
+
+    $('#ConferencesToJoin').on('click', function(e) {
+        e.preventDefault();
+        var EventID =  Number.parseInt($( "#conferencestojoin").value)
+        window.sessionStorage.setItem('EventID', EventID);
+        $.ajax({
+            method: "POST",
+            url: '/addparticipant',
+            contentType: 'application/json;charset=UTF-8',
+            data: JSON.stringify({
+                'username': username,
+                'eventid': EventID,
+                'loginas': loginas
+            }),
+            dataType: "json",
+            success: function(data) {
+                console.log(data)
+            },
+            statusCode: {
+                400: function() {
+                    $('#msg').html('<span style="color: red;">Bad request - invalid credentials</span>');
+                }
+            },
+            error: function(err) {
+                console.log(err);
+            }
+        });
+        var url = "http://localhost:8080/author";
+        $(location).attr('href',url);
     });
 
     $('#logOutButt').on('click', function(e) {
